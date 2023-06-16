e60dd19cfde0afc12d5475d0274f5c2e8c181c71
Paul Kimball <pekimball@verizon.net>
2023-06-15 23:09:45 -0400

    Patched Board.java

--- src/main/java/com/petwaregames/bbw/base/Board.java
+++ src/main/java/com/petwaregames/bbw/base/Board.java
@@ -1,6 +1,8 @@
 package com.petwaregames.bbw.base;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +11,7 @@ import java.util.List;
  * tokens are displayed against a background
  */
 
-public class Board extends JPanel implements Animated {
+public class Board extends JPanel implements Animated, KeyListener {
 
     /**
      * The Board maintains a list of tokens
@@ -28,19 +30,62 @@ public class Board extends JPanel implements Animated {
     public void addToken(Token token){
         gameTokens.add(token);
     }
+
     public void removeToken(Token token){
         gameTokens.remove(token);
     }
 
 
     public void onClockTick(){
+        // this gets called once every tick, before the repainting process happens.
+        // so that the board and its tokens can update their state before the
+        // next redisplay
+
+        for (Token token : gameTokens){
+            token.onClockTick();
+        }
+
+        // prevent the player from moving off the edge of the board sideways
+//        if (pos.x < 0) {
+//            pos.x = 0;
+//        } else if (pos.x >= Board.COLUMNS) {
+//            pos.x = Board.COLUMNS - 1;
+//        }
+//        // prevent the player from moving off the edge of the board vertically
+//        if (pos.y < 0) {
+//            pos.y = 0;
+//        } else if (pos.y >= Board.ROWS) {
+//            pos.y = Board.ROWS - 1;
+//        }
+        repaint();
 
     }
 
 
     public void onPlayerInput(){
+        // prevent the player from disappearing off the board
 
+        // See if the player touched any other Tokens
+
+        // calling repaint() will trigger paintComponent() to run again,
+        // which will refresh/redraw the graphics.
+        repaint();
     }
 
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // this is not used but must be defined as part of the KeyListener interface
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // react to key down events
+       // player.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // react to key up events
+    }
 }
